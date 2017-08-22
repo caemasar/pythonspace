c86157006b6d878ec10d0c36e2b2d6d23255f9f5
Caemasar <caemasar@protonmail.com>
2017-08-22 15:08:23 +0800

    例行更新-->【递归函数】

---
+++ abstest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#python解释器中可以使用from abstest import my_abs导入函数
+
+
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
---
+++ pt3.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#空函数
+
+#如果想定义一个什么事也不做的空函数，可以用pass语句：
+
+def nop():
+    pass
+#pass语句什么都不做，那有什么用？实际上pass可以用来作为占位符，比如现在还没想好怎么写函数的代码，就可以先放一个pass，让代码能运行起来。
+
+#参数检查
+
+#返回多个值
+import math
+
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+
+x, y = move(100, 100, 60, math.pi / 6)
+print(x, y)
+
+r = move(100, 100, 60, math.pi / 6)
+print(r)
+'''
+原来返回值是一个tuple！但是，在语法上，返回一个tuple可以省略括号，
+而多个变量可以同时接收一个tuple，按位置赋给对应的值，所以，
+Python的函数返回多值其实就是返回一个tuple，但写起来更方便。
+'''
+'''
+参数组合
+
+在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，
+这5种参数都可以组合使用。但是请注意，参数定义的顺序必须是：
+必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
+'''
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+
+f1(1, 2)
+f1(1, 2, c=3)
+f1(1, 2, 3, 'a', 'b')
+f1(1, 2, 3, 'a', 'b', x=99)
+f2(1, 2, d=99, ext=None)
+
+args = (1, 2, 3, 4)
+kw = {'d': 99, 'x': '#'}
+f1(*args, **kw)
+
+args = (1, 2, 3)
+kw = {'d': 88, 'x': '#'}
+f2(*args, **kw)
