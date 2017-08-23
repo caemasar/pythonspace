67d823af68d632d59447d94d4077ed236d266b11
Caemasar <caemasar@protonmail.com>
2017-08-23 14:27:06 +0800

    例行提交-->【列表生成式2】

--- pt6.py
+++ pt6.py
@@ -16,3 +16,21 @@ print(L)
 
 # 方法二
 print([x * x for x in range(1, 11)])
+
+print([x * x for x in range(1, 11) if x % 2 == 0])
+
+print([m + n for m in 'ABC' for n in 'XYZ'])
+
+import os
+# 导入os模块，模块的概念后面讲到
+print([d for d in os.listdir('.')])
+# os.listdir可以列出文件和目录
+
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+for k, v in d.items():
+    print(k, '=', v)
+
+print([k + '=' + v for k, v in d.items()])
+
+L = ['Hello', 'World', 'IBM', 'Apple']
+print([s.lower() for s in L])
